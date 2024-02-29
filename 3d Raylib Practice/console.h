d53a82b477b62a6ea4cb6bbf7cb66261f8ab8d16
htdguide <mogilevskiy.nik@gmail.com>
2024-02-29 13:10:34 +1300

    Comments fixes #5

--- 3d Raylib Practice/console.h
+++ 3d Raylib Practice/console.h
@@ -20,26 +20,22 @@ public:
 	int topBar = 20;																																	//Window top bar size (height)
 	int buttonsWidth = 30;																																//Buttons width
 
-	struct textBufferStruct {
+	struct textBufferStruct {																															//textBuffer structure to create array of structs later
 		char text[128] = { 0 };																															//One line of the text
 	};
-	textBufferStruct textBuffer[20];
+	textBufferStruct textBuffer[20];																													//Array of structs, each struct is just a line of the text
 
-	const int textSize = sizeof(textBuffer[0].text);																											//Taking the text size from the struct
-
-	bool exitStatus = false;
-	bool textBoxStatus = false;
+	bool exitStatus = false;																															//Exit status for the console window closing
+	bool textBoxStatus = false;																															//textBox activation status
 	float scaleNum = 166348;																															//This number came from the formula [ScreenWidth * ScreesHeight / desired font size]
 	int fontSize = GetScreenHeight() * GetScreenWidth() / scaleNum;																						//Getting font size 30 with resolution 2736 x 1824
-	string input;																								    									//Input from the keyboard
-	string previousInput;	
-	
+		
 
 	Vector2 backgroundSize;																																//Size of the background box
 	Vector2 backgroundPosition;																															//Position of the background
 	Vector2 position{ 10, 10 };																															//Position of the console
 	Vector2 size = { 800 , 600 };																														//Size of the console
-	Vector2 exitButtonSize = { (float)buttonsWidth, (float)topBar };
+	Vector2 exitButtonSize = { (float)buttonsWidth, (float)topBar };																					//Text button size assigned above
 	
 	int borders = 10;																																	//Borders padding [Choose between 0 and 100]
 	
@@ -50,30 +46,30 @@ public:
 	Rectangle textBox;
 
 
-	bool console() {																																	//Drawing a console
-		stylesSettingsDefault();																														//Applying styles default settings
-		DrawRectangleRounded(Rectangle{ position.x, position.y, size.x, size.y }, 0.01, 25, borderColor);												//Drawing a rectangle behind background to make borders
+	bool console() {																																										//Drawing a console
+		stylesSettingsDefault();																																							//Applying styles default settings
+		DrawRectangleRounded(Rectangle{ position.x, position.y, size.x, size.y }, 0.01, 25, borderColor);																					//Drawing a rectangle behind background to make borders
 		
-		backgroundPosition = { position.x + borders, position.y + borders + 20};																		//Calculation of the textbox coordinates inside the console + 10 pixels of the gap for close/move buttons
-		backgroundSize = { size.x - 2 * borders, size.y - 2 * borders - topBar};																		//Calculation of the textbox size inside the console by using padding
-		DrawRectangle(backgroundPosition.x, backgroundPosition.y, backgroundSize.x, backgroundSize.y, backgroundColor);									//Drawing background rectangle
+		backgroundPosition = { position.x + borders, position.y + borders + 20};																											//Calculation of the textbox coordinates inside the console + 10 pixels of the gap for close/move buttons
+		backgroundSize = { size.x - 2 * borders, size.y - 2 * borders - topBar};																											//Calculation of the textbox size inside the console by using padding
+		DrawRectangle(backgroundPosition.x, backgroundPosition.y, backgroundSize.x, backgroundSize.y, backgroundColor);																		//Drawing background rectangle
 		
-		textBox = { backgroundPosition.x, backgroundPosition.y + backgroundSize.y - textBoxSize, backgroundSize.x, (float)textBoxSize };				//Textbox rectangle
-		DrawRectangleLinesEx(textBox, 3.0, borderColor);																								//Drawing a textbox borders
+		textBox = { backgroundPosition.x, backgroundPosition.y + backgroundSize.y - textBoxSize, backgroundSize.x, (float)textBoxSize };													//Textbox rectangle
+		DrawRectangleLinesEx(textBox, 3.0, borderColor);																																	//Drawing a textbox borders
 		
-		Rectangle exitButton{ backgroundPosition.x + backgroundSize.x - exitButtonSize.x, backgroundPosition.y  - exitButtonSize.y, exitButtonSize.x, exitButtonSize.y};
-		if (GuiButton(exitButton, " ")) {
-			exitStatus = true;
+		Rectangle exitButton{ backgroundPosition.x + backgroundSize.x - exitButtonSize.x, backgroundPosition.y  - exitButtonSize.y, exitButtonSize.x, exitButtonSize.y};					//Exit button rectangle for drawing
+		if (GuiButton(exitButton, " ")) {																																					//Drawing a button
+			exitStatus = true;																																								//If button pressed, exit
 		}
-		if(GuiTextBox(textBox, textBuffer[0].text, 128, textBoxStatus)) textBoxInputHandler();
-		mouseClickHandler();
-		return exitStatus;
+		if(GuiTextBox(textBox, textBuffer[0].text, 128, textBoxStatus)) textBoxInputHandler();																								//Drawing a textbox, if pressed textBoxInputHandler method works
+		mouseClickHandler();																																								//Mouse click handler to catch mouse clicks in different places
+		return exitStatus;																																									//Returning exit status
 	}
 
-	void mouseClickHandler() {
-		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+	void mouseClickHandler() {																																								//Handling mouse clicks
+		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {																																			//If left button pressed
 
-			if (CheckCollisionPointRec(GetMousePosition(), textBox)) {
+			if (CheckCollisionPointRec(GetMousePosition(), textBox)) {																														//If it pressed positioning on textBox rectangle, enable textbox
 				textBoxStatus = true;
 			}
 			else textBoxStatus = false;
@@ -104,17 +100,17 @@ public:
 	
 	}
 
-	void textBoxInputHandler() {																										
-		commands(textBuffer[0].text);
-		for (int i = 0; i++; i < sizeof(textBuffer) - 1) {																							//Moving all of the data to the upper line
-			strcpy(textBuffer[i+1].text, textBuffer[i].text);
+	void textBoxInputHandler() {																														//Text input handler, triggers all of the commands and saves previous imputs														
+		commands(textBuffer[0].text);																													//Initiatind command from commands list 
+		for (int i = 0; i++; i < sizeof(textBuffer) - 1) {																								//Moving all of the data to the upper line
+			strcpy(textBuffer[i+1].text, textBuffer[i].text);																							
 		}
-		strcpy(textBuffer[0].text, "");																												//Erasing main input line																						
+		strcpy(textBuffer[0].text, "");																													//Erasing main input line																						
 	}
 
-	void commands(string input) {
+	void commands(string input) {																														//Commands list 
 
-		if (input == "scene3d") {
+		if (input == "scene3d") {																														//3d scene creation command
 			scene3D();
 		}
 
